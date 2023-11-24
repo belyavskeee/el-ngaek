bf221f2a2e92dd7b8b615f435b22502d8438d8ea
belyavskeee <belayvskijmatveyj309@gmail.com>
2023-11-24 19:28:35 +0300

    new changes

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+  });
